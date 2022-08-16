b9f3df6f4f8677ad3461fa52aaab7551682df046
NexVRStudios <99061870+NexVRStudios@users.noreply.github.com>
2022-08-16 09:59:36 -0400

    I used a YouTube tutorial for these

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CSharp_Youtube_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Multiplyer"; 
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            //looks
+
+            int num01;
+            int num02;
+
+            Console.Write("First Number: ");
+
+            num01 = Convert.ToInt32(Console.ReadLine() );
+
+            //recieve first number
+
+            Console.Write("Second Number: ");
+
+            num02 = Convert.ToInt32(Console.ReadLine() );
+
+            //recieve second number
+
+            int result = num01 * num02;
+
+            Console.Write("Result=" + result);
+
+            Console.Write("\n\nPress Any Key to Close");
+            
+            Console.ReadKey();
+            //closes on key press
+        }
+    }
+}
